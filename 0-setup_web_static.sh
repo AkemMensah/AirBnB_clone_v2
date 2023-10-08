4c14f14b67e80f2f86d1cceca19f8da2c474e76b
AkemMensah <141175emmanuel@gmail.com>
2023-10-08 20:23:40 +0000

    resolve

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,40 @@
 #!/usr/bin/env bash
 # Bash script that sets up web servers for the deployment of web_static
 
-apt-get update
+sudo apt-get update
 
 # Install Nginx if it not already installed
-apt-get -y install nginx
+sudo apt-get -y install nginx
 
 # Create the folders /data /web_static /releases /test/ if it doesn’t already exist
-mkdir -p ~/data/web_static/releases/test/
+sudo mkdir -p ~/data/web_static/releases/test/
 
 # Create the folder /data/web_static/shared/ if it doesn’t already exist
-mkdir -p ~/data/web_static/shared/
-
+sudo mkdir -p ~/data/web_static/shared/
+touch ~/data/web_static/releases/test/index.html
 # Create a fake HTML file /data/web_static/releases/test/index.html
-echo '<html>
+sudo echo '<html>
   <head>
   </head>
   <body>
     Alx SE School
   </body>
-</html>' > /data/web_static/releases/test/index.html
+</html>' > ~/data/web_static/releases/test/index.html
 
 # Create a symbolic link /data/web_static/current linked to the\
 # /data/web_static/releases/test/ folder. If the symbolic link already\
 # exists, it should be deleted and recreated every time the script is ran. 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf ~/data/web_static/releases/test/ ~/data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group (
 # assume this user and group exist). This should be recursive; everything
 # inside should be created/owned by this user/group.
-chown -hR ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu ~/data/
 
 # Update the Nginx configuration to serve the content of
 # /data/web_static/current/ to hbnb_static
 # (ex: https://mydomainname.tech/hbnb_static).
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '51 i \\n\tlocation /hbnb_static {\n\talias ~/data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 
 # Restart Nginx after updating the configuration:
-service nginx restart
+sudo service nginx restart
