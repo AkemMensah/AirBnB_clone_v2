392dc449e845b950dbb14fcb96fa94c03051164e
AkemMensah <141195emmanuel@gmail.com>
2023-10-09 11:09:05 +0000

    resolve

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 # Bash script that sets up web servers for the deployment of web_static
 
-sudo apt-get update
+apt-get update
 
 # Install Nginx if it not already installed
-sudo apt-get -y install nginx
+apt-get -y install nginx
 
 # Create the folders /data /web_static /releases /test/ if it doesn’t already exist
-sudo mkdir -p ~/data/web_static/releases/test/
+mkdir -p ~/data/web_static/releases/test/
 
 # Create the folder /data/web_static/shared/ if it doesn’t already exist
-sudo mkdir -p ~/data/web_static/shared/
+mkdir -p ~/data/web_static/shared/
 touch ~/data/web_static/releases/test/index.html
 # Create a fake HTML file /data/web_static/releases/test/index.html
-sudo echo '<html>
+echo '<html>
   <head>
   </head>
   <body>
@@ -24,33 +24,40 @@ sudo echo '<html>
 # Create a symbolic link /data/web_static/current linked to the\
 # /data/web_static/releases/test/ folder. If the symbolic link already\
 # exists, it should be deleted and recreated every time the script is ran. 
-sudo ln -sf ~/data/web_static/releases/test/ ~/data/web_static/current
+ln -sf ~/data/web_static/releases/test/ ~/data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group (
 # assume this user and group exist). This should be recursive; everything
 # inside should be created/owned by this user/group.
-sudo chown -hR ubuntu:ubuntu ~/data/
-
+chown -hR ubuntu:ubuntu ~/data/
+echo "Keep going"
+ls /data/
+pwd /data/
+pwd ~/data/
 # Update the Nginx configuration to serve the content of
 # /data/web_static/current/ to hbnb_static
 # (ex: https://mydomainname.tech/hbnb_static).
-#sudo sed -i '51 i \\n\tlocation /hbnb_static {\n\talias ~/data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-echo 'server {
+#sudo sed -i ' i \\n\tlocation /hbnb_static {\n\talias ~/data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+
+printf %s "server {
     listen 80 default_server;
-            listen [::]:80 default_server;
-	                add_header X-Served-By 316263-web-01;
-			                root   ~/data/web_static/current;
-					                    index  index.html index.htm;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-							                            location /redirect_me {
-										                                    return 301 https//YouTube.com/;
-														                                        }
-
-																		                                error_page 404 /404.html;
-																						                                    location /404 {
-																										                                              root /var/www/html;
-																															                                                      internal;
-																																					                                                          }
-																																											                                      }' > /etc/nginx/sites-available/default
+    location /hbnb_static {
+            alias /data/web_static/current;
+	     index index.html index.htm;
+     }
+    location /redirect_me {
+            return 301 http://google.com/;
+	        }
+   error_page 404 /404.html;
+   location /404 {
+         root /var/www/html;
+	 internal;
+          }
+   }" > /etc/nginx/sites-available/default
 # Restart Nginx after updating the configuration:
-sudo service nginx restart
+service nginx restart
