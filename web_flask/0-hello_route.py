647e0335cf7747af1501a18c2ccde6b228989de1
AkemMensah <141195emmanuel@gmail.com>
2023-11-15 19:22:25 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+#Define route for the root URL '/'
+#@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_world():
-    """ Returns some text. """
+    """ Returns the text Hello HBNB! """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
+    #Starts the Flask dev. server
+    #Listen on all available network interface (0.0.0.0) and port 5000
     app.run(host='0.0.0.0', port=5000)
